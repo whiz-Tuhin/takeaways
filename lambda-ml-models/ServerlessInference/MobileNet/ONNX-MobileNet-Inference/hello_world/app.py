8ba23c1d7bb85e9f77296b4dfe94fa7f86d613f7
aakashkhochare <aakashkhochare@gmail.com>
2022-11-17 16:15:35 +0530

    Now using PIL instead of cv2 for reading images (MobileNet)

--- lambda-ml-models/ServerlessInference/MobileNet/ONNX-MobileNet-Inference/hello_world/app.py
+++ lambda-ml-models/ServerlessInference/MobileNet/ONNX-MobileNet-Inference/hello_world/app.py
@@ -2,7 +2,9 @@ import json
 import base64
 import onnxruntime
 import numpy as np
-import cv2
+from PIL import Image
+from io import BytesIO
+# import cv2
 from collections import namedtuple
 
 
@@ -43,9 +45,10 @@ def run_model(model_path, img):
 
 def decode_base64(data):
     img = base64.b64decode(data)
-    img = cv2.imdecode(np.fromstring(img, np.uint8), cv2.IMREAD_COLOR)
-    img = cv2.resize(img, (224, 224))
-    img = img.transpose((2,0,1))
+    # img = cv2.imdecode(np.fromstring(img, np.uint8), cv2.IMREAD_COLOR)
+    # img = cv2.resize(img, (224, 224))
+    img = np.asarray(Image.open(BytesIO(base64.b64decode(data))))[:, :, [2, 1, 0]]
+    img = img.transpose((2, 0, 1))
     img = img.reshape(1, 3, 224, 224)
     return img
 
